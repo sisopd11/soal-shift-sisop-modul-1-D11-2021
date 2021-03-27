24d6da34570a1681d1949324c725b1322e69065c
Afifah Nur Sabrina <75364000+afifahsabrina@users.noreply.github.com>
2021-03-27 20:20:29 +0700

    Create soal1.sh
    poin a - c : 👍🏻 
    poin d : progress buat file sama check error message &sum for each message

---
+++ soal1/soal1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#No1_a
+#Ambil data kata depan Error or Info sampe username dari file syslog.log
+grep -o '[E|I].*' syslog.log
+
+#No1_b
+#Ambil data log Error tapi bagian username di cut, urut abc, sama hitung banyak errornya
+grep -o 'E.*' syslog.log | cut -d"(" -f 1| sort | uniq -c
+
+#No1_c
+#Ambil data log error cuma jumlah error for each username
+echo Error:
+grep -o 'E.*' syslog.log | cut --complement -d"(" -f 1 | cut -d")" -f 1 | sort | uniq -c
+#Ambil data log info cuma jumlah info for each username
+echo Info:
+grep -o 'I.*' syslog.log | cut --complement -d"(" -f 1 | cut -d")" -f 1 | sort | uniq -c
+
+#No1_d
+#File error_message #OK
+echo 'Error,Count' > 'error_message.csv'
+
+#Ambil error messages sama jumlah #FAIL
+grep -o 'E.*' syslog.log | sort | uniq -c | sort -nr while read myfile
+#Divide error sama jumlah
+do
+	#ambil error
+	error=($(echo $myfile | cut =d " " -f 2-)
+	#ambil jumlah
+	count=($(echo $myfile | cut -d " " -f 1 )
+	echo "$error, $count"
+done >> 'error_message.csv' 
+
+#No1_e
+#File user_statistic.csv #OK
+echo 'Username,INFO,ERROR' > 'error_message.csv'

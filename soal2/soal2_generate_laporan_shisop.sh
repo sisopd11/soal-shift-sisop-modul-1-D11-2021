4ddb971d2d3d74389fbb3c69a0b97550ed4db06e
sisopd11 <80894892+sisopd11@users.noreply.github.com>
2021-03-27 19:05:25 +0700

    Add files via upload

---
+++ soal2/soal2_generate_laporan_shisop.sh
@@ -0,0 +1,54 @@
+export LC_ALL=C
+awk '
+BEGIN{FS="\t"}
+{
+  profit=$21;
+  costPrice=$18-$21;
+ profitpersentase=profit/costPrice*100
+   if(maks<=profitpersentase){
+     maks=profitpersentase
+     RowID=$1s}
+}
+END {
+ printf("Transaksi terkahir dengan profit persentase terbesar yaitu %d dengan persentase %d%%\n", RowID, maks)
+} ' /home/dewi/SISOP/praktikum1/Laporan-TokoShiSop.tsv >> hasil.txt
+
+#2b
+export LC_ALL=
+awk '
+BEGIN{FS="\t"}
+{
+  tahun=$2;
+  city=$10;
+  if(tahun~"2017" && city=="Albuquerque"){
+    arrlist[$7]++
+  }
+}
+END{
+  print "\nDaftar nama customer di Alburquerque pada tahun 2017 antara lain :"
+  for(customerName in arrlist)
+  {print customerName}
+}' /home/dewi/SISOP/praktikum1/Laporan-TokoShiSop.tsv >> hasil.txt
+
+#2c
+export LC_ALL=C
+awk '
+BEGIN{FS="\t"}
+{
+   if(NR!=1){
+    segment[$8]++
+  }
+}
+END {
+  minSales=10000
+  for(temp in segment){
+    if(minSales > segment[temp]){
+      minSales = segment[temp]
+      sum = temp;
+    }
+  }
+  printf("\nTipe segment customer yang penjualannya paling sedikit adalah %s dengan segment %.1f\n", sum, minSales)
+}' /home/dewi/SISOP/praktikum1/Laporan-TokoShiSop.tsv >> hasil.txt
+
+
+

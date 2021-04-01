3bebc49e1063ca404883743a1ef72de54f1675b6
Afifah Nur Sabrina <75364000+afifahsabrina@users.noreply.github.com>
2021-04-01 21:21:00 +0700

    Revisi e

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -40,6 +40,50 @@ done >> error_message.csv
 rm message.csv
 rm count.csv
 
-#No1_e
-#File user_statistic.csv #OK
-echo 'Username,INFO,ERROR' > 'error_message.csv'
+#e
+# File user_statistic.csv #OKKK
+printf 'Username, INFO, ERROR\n' > user_statistic.csv
+
+# Save user ke user.csv
+cat syslog.log | cut -d'(' -f2- | cut -d')' -f1 | sort | uniq -c | sort -nr | tr -d '[0-9]' | sed -e 's/^[[:space:]]*//' > user.csv
+# Save hasil count error for each user ke count_error.csv
+grep "E.*" syslog.log | cut -d'(' -f2- | cut -d')' -f1 | sort | uniq -c | sort -nr | grep -Eo '[0-9]{1,}' > count_error.csv
+# Save user yang memiliki error ke user_error.csv
+grep "E.*" syslog.log | cut -d'(' -f2- | cut -d')' -f1 | sort | uniq -c | sort -nr | tr -d '[0-9]' | sed -e 's/^[[:space:]]*//' > user_error.csv
+# Save hasil count info for each user ke count_info.csv
+grep "I.*" syslog.log | cut -d'(' -f2 | cut -d')' -f1 | sort | uniq -c | sort -nr | grep -Eo '[0-9]{1,}' > count_info.csv
+# Save user yang memiliki info ke user_info.csv
+grep "I.*" syslog.log | cut -d'(' -f2 | cut -d')' -f1 | sort | uniq -c | sort -nr | tr -d '[0-9]' | sed -e 's/^[[:space:]]*//' > user_info.csv
+while read username
+do
+  user="$username"
+  info=0
+  error=0
+  #compare user yang memiliki error dengan data user only
+  paste count_error.csv user_error.csv | (while read counterror usererror
+  do
+    if [ "$user" == "$usererror" ]
+     then 
+         error=$counterror 
+         break
+    fi
+  done
+  #compare user yang memiliki info dengan data user only 
+  paste count_info.csv user_info.csv | (while read countinfo userinfo
+  do
+    if [ "$user" == "$userinfo" ]
+     then 
+         info=$countinfo 
+         break
+    fi
+  done
+
+ printf "$user,$info,$error\n" >> user_statistic.csv))
+done < user.csv
+
+#Hapus file temporary
+rm user.csv
+rm count_error.csv
+rm user_error.csv
+rm count_info.csv
+rm user_info.csv

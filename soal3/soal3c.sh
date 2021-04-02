7c6282b02abb3d02f9989704315b828c410dc6cd
sisopd11 <80894892+sisopd11@users.noreply.github.com>
2021-04-02 14:35:35 +0700

    Update soal3c.sh

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -1,23 +1,52 @@
 #!/bin/bash
 
-CAT_LINK="https://loremflickr.com/320/240/bunny"
-i=1
+
 tanggal=`date +%d-%m-%Y`
-mkdir "Kelinci_$tanggal"
-
-while [ $i -le 23 ]
-do	
-	wget -N --content-disposition $CAT_LINK -a Foto.txt
-	i=$((i+1))
-done
+CURDAY=`date +%d`
 
 
-i=1
+if [ $(( $CURDAY % 2 )) -eq 1 ]
+then
+	CAT_LINK="https://loremflickr.com/320/240/bunny"
+	mkdir "Kelinci_$tanggal"
+	i=1
 
-for name in `ls | grep jpg$`
-do
-	mv "$name" "Kelinci_$tanggal"/"Koleksi_$i.jpg"
-	i=$((i+1))
-done
+	while [ $i -le 23 ]
+	do	
+		wget -N --content-disposition $CAT_LINK -a Foto.txt
+		i=$((i+1))
+	done
 
-mv Foto.txt "Kelinci_$tanggal"/
+
+	i=1
+
+	for name in `ls | grep jpg$`
+	do
+		mv "$name" "Kelinci_$tanggal"/"Koleksi_$i.jpg"
+		i=$((i+1))
+	done
+
+	mv Foto.txt "Kelinci_$tanggal"/
+
+else
+	CAT_LINK="https://loremflickr.com/320/240/kitten"
+	mkdir "Kucing_$tanggal"
+	i=1
+
+	while [ $i -le 23 ]
+	do	
+		wget -N --content-disposition $CAT_LINK -a Foto.txt
+		i=$((i+1))
+	done
+
+
+	i=1
+
+	for name in `ls | grep jpg$`
+	do
+		mv "$name" "Kucing_$tanggal"/"Koleksi_$i.jpg"
+		i=$((i+1))
+	done
+
+	mv Foto.txt "Kucing_$tanggal"/
+fi

73df279a9b365bc5fe254f39b1da1599d9873610
sisopd11 <80894892+sisopd11@users.noreply.github.com>
2021-04-02 14:35:00 +0700

    Add files via upload

---
+++ soal3/soal3e.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+unzip -P `date +%m%d%Y` -o /home/avind/Desktop/Soal3_shift1_sisopD/Koleksi.zip -d /home/avind/Desktop/Soal3_shift1_sisopD
+rm /home/avind/Desktop/Soal3_shift1_sisopD/Koleksi.zip
